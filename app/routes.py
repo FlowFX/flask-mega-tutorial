2d5963671b3fc30599a44c12e4c34c0bf17322c9
Florian Posdziech <hallo@flowfx.de>
2017-12-12 12:38:23 -0600

    Finish Chapter 2
    https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-ii-templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Florian'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Hamburg!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Matrix movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
